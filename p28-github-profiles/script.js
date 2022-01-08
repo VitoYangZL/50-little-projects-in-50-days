9a799406641f16fdf230b7c63135f8c5e9af85fd
Vito Yang <yutcpp41100202@gmail.com>
2022-01-08 15:44:53 +0800

    p28 done.

---
+++ p28-github-profiles/script.js
@@ -0,0 +1,93 @@
+const APIURL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+async function getUser(username) {
+  try {
+    const { data } = await axios.get(APIURL + username);
+
+    createUserCard(data);
+    getRepos(username);
+  } catch (err) {
+    if (err.response.status == 404) {
+      createErrorCard("No profile with this username");
+    }
+  }
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+    <div class="card">
+        <div class="img-container">
+            <img
+            src="${user.avatar_url}"
+            class="avatar"
+            alt="${user.name}"
+            />
+        <div class="user-info">
+            <h2>${user.name}</h2>
+            <p>
+            ${user.bio}
+            </p>
+            <ul>
+            <li>${user.followers} <strong>Followers</strong></li>
+            <li>${user.following} <strong>Following</strong></li>
+            <li>${user.public_repos} <strong>Repos</strong></li>
+            </ul>
+        <div id="repos">
+          
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+  main.innerHTML = cardHTML;
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + "/repos?sort=created");
+
+    addReposToCard(data);
+  } catch (err) {
+    console.log(err);
+    createErrorCard("Problem fetching repo");
+  }
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class="card">
+        <div class="img-container">
+            <h1>${msg}</h1>
+        </div>
+    </div>
+    `;
+  main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById("repos");
+
+  repos.slice(0, 5).forEach((repo) => {
+    const repoEl = document.createElement("a");
+    repoEl.classList.add("repo");
+    repoEl.href = repo.html_url;
+    repoEl.target = "_blank";
+    repoEl.innerText = repo.name;
+
+    reposEl.appendChild(repoEl);
+  });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+    search.value = "";
+  }
+});
